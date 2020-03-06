c7c970050b7d4208ec20ec0f70087db402cd3f91
Artandrey <artandrey7777@gmail.com>
2020-03-06 19:25:15 +0200

    add-shmorze

---
+++ shmorze/index.js
@@ -0,0 +1,56 @@
+"use strict"
+
+const sypher = ["ьь","Ьь","Ъ","ъ","ъъ", "ьЪ", "Ъь", "ь","ЪЪ", "ЪъЬ", "ЬъЬ", "Ъь", "Ьъъ", "Ььь","Ъъь", "ЬЬъ", "ЪЬъ", "ЬЬЬ", "ЪЪЪ", "ььь", "Ь", "ъъъ","ъЪ","ьЬ","ъЪь","ьЬъ","Е","ъЕ","е","ЬЕЪ","ьЕъ","ЪЕЪ", " "];
+const letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'," "];
+const numberOfLetter =[];
+const syphredMessageArray = [];
+let message;
+let fraze = "андрей";
+
+
+
+
+const doArrayOfNumbers = function(fraze, splitMode, usedArray) {
+    let lettersOfFraze = fraze.split(splitMode);
+for (let i = 0; i < lettersOfFraze.length; i++) {
+    for (let j = 0; j < usedArray.length; j++) {
+        if (lettersOfFraze[i] == (usedArray[j])) {
+            numberOfLetter.push(j);     
+    }
+}
+}
+return(numberOfLetter);
+}
+
+
+const createSypher = function(numberOfLetter, syphredMessageArray, usedArray) {
+for (let c = 0; c < numberOfLetter.length; c++) {
+
+    syphredMessageArray.push(usedArray[numberOfLetter[c]]);
+}
+return(syphredMessageArray);
+}
+
+
+const doSroke = function(syphredMessageArray, usedJoin){
+message = syphredMessageArray.join(usedJoin);
+return(message);
+}
+
+
+
+const toSypher = function() {
+    doArrayOfNumbers(fraze, "", letters);
+    createSypher(numberOfLetter, syphredMessageArray,  sypher);
+    doSroke(syphredMessageArray, " ")
+}
+
+
+const toUnsypher = function() {
+    doArrayOfNumbers(fraze, " ", sypher);
+    createSypher(numberOfLetter, syphredMessageArray, letters);
+    doSroke(syphredMessageArray, "")
+}
+
+console.log(message);
+
