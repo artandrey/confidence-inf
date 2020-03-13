7c757f34ccca1e790a6ebdd67de2e73f5821328b
Artandrey <artandrey7777@gmail.com>
2020-03-13 12:52:50 +0200

    add easter egg fix mobile

--- shmorze2/easter-egg.js
+++ shmorze2/easter-egg.js
@@ -3,3 +3,12 @@
 
 
 
+let easerEggInput = document.querySelector(".input");
+input.addEventListener("change", () => {easerEggRun(input.value);});
+const easerEggRun = function(value) {
+    if(value == "/matrix") {
+        easterEggBox.style.display = "block";
+    }
+}
+let easterEggBox = document.querySelector(".easter-egg");
+console.log(easterEggBox)
