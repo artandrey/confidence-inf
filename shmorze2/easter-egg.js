b07f34302f2927c2cfd0cf3d40e18a44bfb113ed
Artandrey <artandrey7777@gmail.com>
2020-03-13 15:26:27 +0200

    add easter egg

--- shmorze2/easter-egg.js
+++ shmorze2/easter-egg.js
@@ -1,6 +1,8 @@
 "use-strict"
 
 
+let audio = new Audio();
+audio.src = 'last-chance.mp3';
 
 
 let easerEggInput = document.querySelector(".input");
@@ -8,7 +10,24 @@ input.addEventListener("change", () => {easerEggRun(input.value);});
 const easerEggRun = function(value) {
     if(value == "/matrix") {
         easterEggBox.style.display = "block";
+        audio.play();
     }
 }
 let easterEggBox = document.querySelector(".easter-egg");
-console.log(easterEggBox)
+let morpheusBox = document.querySelector(".morpheus-box");
+let canvas = document.querySelector(".canvas");
+let skipBtn = document.querySelector(".skip-btn")
+const skip = function() {
+    morpheusBox.style.display = "block";
+    canvas.style.display = "none";
+    skipBtn.style.display = "none";
+}
+
+let red = document.querySelector(".red");
+let blue = document.querySelector(".blue");
+let gendalf = document.querySelector(".gendalf");
+const leave = function() {
+    location.href=location.href;
+}
+blue.addEventListener("click", () => {easterEggBox.style.display = "none"; location.href=location.href;})
+red.addEventListener("click", () => {gendalf.style.display = "block"; audio.pause();})
