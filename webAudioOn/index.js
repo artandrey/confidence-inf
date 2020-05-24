74be8a58dc101d354583629df2a856efdcc015dc
Artandrey <artandrey7777@gmail.com>
2020-05-24 14:19:01 +0300

    12

--- webAudioOn/index.js
+++ webAudioOn/index.js
@@ -1,7 +1,7 @@
 
-const audio = new buzz.sound('b4.mp3', {webAudioApi: true, preload: true});
 
 document.querySelector('button').addEventListener('click', () => {
+    const audio = new buzz.sound('b4.mp3', {webAudioApi: true, preload: true});
     audio.play()
 })
 console.log(12);
